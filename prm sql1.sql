743645b93b79201617b61bd3827a8ea1f364f7c7
Shubham goyal <111068630+Goyal869@users.noreply.github.com>
2022-11-15 22:29:57 +0530

    Update prm sql1.sql

--- prm sql1.sql
+++ prm sql1.sql
@@ -2,7 +2,7 @@ create database country;
 use country;
 
 create table countries(
-country_id  varchar(10),
-country_name varchar(10),
+country_id  varchar(2),
+country_name varchar(40),
 check (country_name in ("Italy","India","China")),
 region_id decimal(10,0));
