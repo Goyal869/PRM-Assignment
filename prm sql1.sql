c3478ff4bfda1795c7e5348b006d54f09149fabf
Shubham goyal <111068630+Goyal869@users.noreply.github.com>
2022-11-15 15:06:37 +0530

    Add files via upload

---
+++ prm sql1.sql
@@ -0,0 +1,8 @@
+create database country;
+use country;
+
+create table countries(
+country_id  varchar(10),
+country_name varchar(10),
+check (country_name in ("Italy","India","China")),
+region_id int);
